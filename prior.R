5e027a5bcf98ba471a6ffb95b76bbfb2e2d30d97
mariazuba <mariazuba@gmail.com>
2026-01-27 11:11:54 +0100

    cambios prior

--- prior.R
+++ prior.R
@@ -46,52 +46,56 @@ priors_run5 <- list(
   priors = NULL
 )
 
-# RUN6 — No prior on logbkfrac (n kept at Schaefer-type default)
+# RUN6 — prior_logn Tighter Schaefer
 priors_run6 <- list(
-  name = "RUN6_no_logbkfrac_logn",
+  name = "RUN6_prior_logn_tighter",
   priors = list(
-    logn  = c(log(2), 0.5, 1),   # prior on n only (Schaefer-type)
-    logr  = c(log(0.5), 0.2, 1),
-    logsdb = c(log(0.2), 0.5), # prior on catch process uncertainty
-    logsdc = c(log(3), 0.5, 1), # prior decrease catch sd
-    logdf = c(log(0.1), 0.2, 1) # Decresase f error sd
-  )
-)
+    logn  = c(log(2), 0.5, 1)))   # prior on n only (Schaefer-type))
 
-# RUN7 — Prior on index observation uncertainty (CV ≈ 0.2)
+# RUN7 — fixing log_n
 priors_run7 <- list(
-  name = "RUN7_index_cv",
-  priors = list(
-    # logbkfrac = c(log(0.5), 0.2, 1),
-    logsdi = c(log(0.1), 0.2, 1),
-    logn      = c(log(2),   0.5, 1),
-    logr      = c(log(0.5), 0.2, 1),
-    logsdb = c(log(0.2), 0.5),
-    logsdc = c(log(3), 0.5, 1),
-    logdf = c(log(0.1), 0.2, 1)
-  )
+  name = "RUN7_fixing_logn",
+  ini = list(
+    logn = log(2)),
+  phases =list(
+    logn=-1)
 )
 
-# RUN8 — Alpha and beta stabilising priors disabled
+# RUN8.0 — prior_logn Tighter Schaefer and prior logr
 priors_run8 <- list(
-  name = "RUN8_no_alpha_beta",
+  name = "RUN8_prior_logn_tighter_logr",
   priors = list(
-    logbkfrac = c(log(0.5), 0.2, 1),
-    logalpha = c(1, 1, 0), # disable
-    logbeta  = c(1, 1, 0), # disable , to activate, change to (0,0,0)
-    logn      = c(log(2),   0.5, 1),
-    logr      = c(log(0.5), 0.2, 1),
-    logsdb = c(log(0.2), 0.5),
-    logsdc = c(log(3), 0.5, 1),
-    logdf = c(log(0.1), 0.2, 1)
-  )
-)
+    logn     = priors_run6$priors$logn,
+    logr      = c(log(0.5), 0.2, 1)
+  ))
+
+# RUN8.1 — fixing log_n and prior logr (no usar)
+priors_run8.1 <- list(
+  name = "RUN8_fixing_logn_logr",
+  priors = list(
+    logr      = c(log(0.5), 0.2, 1)
+  ),
+  ini = list(
+    logn = log(2)),
+  phases =list(
+    logn=-1))
+
+
+# RUN9— prior_logn Tighter Schaefer and prior logr and logbkfrac
+priors_run9 <- list(
+  name = "RUN8_prior_logn_tighter_logr_logbk",
+  priors = list(
+    logn     = priors_run6$priors$logn,
+    logr      = priors_run8$priors$logr,
+    logbkfrac = c(log(0.5), 0.2, 1)
+  ))
 
 scenarios_priors <- list(
-  # RUN5 = priors_run5,
-  # RUN6 = priors_run6,
-  # RUN7 = priors_run7,
-  RUN8 = priors_run8
+  RUN5 = priors_run5,
+  RUN6 = priors_run6,
+  RUN7 = priors_run7,
+  RUN8 = priors_run8,
+  RUN9 = priors_run9
 )
 
 #guardar Rdata
